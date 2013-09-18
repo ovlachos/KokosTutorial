ebee30753fa43afa9d2c6a5d71307585e563f6eb
Orestis Vlachos <ovlachos@gmail.com>
2013-09-18 12:07:51 +0300

    original code pasted

--- KokosTutorial/KokosTutorial.cpp
+++ KokosTutorial/KokosTutorial.cpp
@@ -1,11 +1,56 @@
-// KokosTutorial.cpp : Defines the entry point for the console application.
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
 //
 
 #include "stdafx.h"
+using namespace std;
 
+
+class Point{
+public:
+	int x;
+	int y;
+
+	int add()
+	{
+		return x + y;
+	}
+
+};
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	int a = 5;
+
+	cout<< " a: " << a << endl;
+
+	int *p = &a;
+
+	*p = 666;
+
+	cout << " a: " << a << endl;
+	
+	(*p) =45;
+	
+	cout << " a: " << a << endl;
+
+	cout << "-------------" << endl;
+
+	Point po;
+
+	po.x = 6;
+	po.y = 3;
+
+	cout << " po: " << po.add() << endl;
+
+
+	Point* p2 = &po;
+
+	// p2->     =       (*p2).
+
+	cout << " po: " << p2->add() << endl;
+
+	char fff;
+	cin >> fff;
 	return 0;
 }
 
